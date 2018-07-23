348da1ed4c6b5e587058b3412c37c22cef76b385
Rob Bissell <roblincolnbissell@gmail.com>
2018-07-23 14:59:57 -0500

    moved styles to own folder, add logo, add burger menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/d2-circle-text.svg';
+import './styles/App.css';
+import { slide as Menu } from 'react-burger-menu'
 
 class App extends Component {
+
+  showSettings (event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="container">
+        <header className="row">
+          <div className="">
+            <img src={logo} className="top-logo" alt="logo" />
+          </div>
+            <Menu>
+              <a id="nav-home" className="nav-link">Home</a>
+              <a id="nav-phil" className="nav-link">Philosophy/Inspiration</a>
+            </Menu>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
