ef183df6507e63a461641e1e7a59c9f779c6eed2
Rob Bissell <roblincolnbissell@gmail.com>
2018-07-23 16:09:00 -0500

    working on header styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './images/d2-circle-text.svg';
 import './styles/App.css';
+import './styles/burger-style.css'
 import { slide as Menu } from 'react-burger-menu'
 
 class App extends Component {
@@ -11,15 +12,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <header className="row">
-          <div className="">
-            <img src={logo} className="top-logo" alt="logo" />
-          </div>
-            <Menu>
-              <a id="nav-home" className="nav-link">Home</a>
-              <a id="nav-phil" className="nav-link">Philosophy/Inspiration</a>
-            </Menu>
+      <div id="main">
+        <header>
+            <div className="container-fluid">
+            <div className="row">
+              <div className="col-xs-2 col-xs-push-2">
+                <nav>
+                  <Menu
+                    id={ "navbar" }
+                    burgerButtonClassName={ "my-class" }
+                  >
+                    <a id="nav-home" className="nav-link">Home</a>
+                    <a id="nav-phil" className="nav-link">Philosophy/Inspiration</a>
+                  </Menu>
+                </nav>
+              </div>
+              <div className="col-xs-10 col-xs-pull-10">
+                <img src={logo} className="top-logo" alt="D2 Architecture" />
+              </div>
+            </div>
+            </div>
         </header>
       </div>
     );
