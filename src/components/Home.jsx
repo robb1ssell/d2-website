dad0d8c42b485602eb92e871e590ec31e66d393d
Rob Bissell <roblincolnbissell@gmail.com>
2018-07-24 15:20:20 -0500

    added states and props to homepage image gallery - working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,29 @@ import homePageImages from '../data/homePageImages'
 import "../../node_modules/react-image-gallery/styles/css/image-gallery.css";
 
 class Home extends Component {
+  state = {
+    autoPlay: true,
+    showFullscreenButton: false,
+    showGalleryFullscreenButton: false,
+    showPlayButton: false,
+    showGalleryPlayButton: false,
+    showNav: false,
+    slideDuration: 1000,
+    slideInterval: 4000,
+  }
   render() {
     return (
       <div id="home-page-gallery">
           <ImageGallery
             items={homePageImages}
+            showFullscreenButton={this.state.showFullscreenButton && this.state.showGalleryFullscreenButton}
+            showPlayButton={this.state.showPlayButton && this.state.showGalleryPlayButton}
+            showThumbnails={this.state.showThumbnails}
+            showIndex={this.state.showIndex}
+            showNav={this.state.showNav}
+            slideDuration={parseInt(this.state.slideDuration)}
+            slideInterval={parseInt(this.state.slideInterval)}
+            autoPlay={this.state.autoPlay}
           />
       </div>
     );
