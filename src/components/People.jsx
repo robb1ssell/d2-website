0ecc4ea4cfc4a3ecab739a0787683d2c2b66ad3a
Rob Bissell <roblincolnbissell@gmail.com>
2018-07-31 00:23:16 -0500

    started first people page, leaders

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -3,8 +3,72 @@ import React, { Component } from 'react';
 class People extends Component {
   render() {
     return (
-      <div>
-        <h1>People</h1>
+      <div className='row' id='people-master'>
+      <h1 className='col-sm-12'>People</h1>
+        <div className="col-md-6">
+          <img 
+            src="https://lh3.googleusercontent.com/Yibfvke12f_v9ipti51T0uUqBm5QjDzZEqVO0eRiVqbkfwbMYtYQY1jPuCDE12BV3VTqs1SgGBmKclquCNTnrioCwusB3lD_Sz-dIxhceIlVvjVP2oy7YLaovtOelWqmFCaR01F-ZvLH2Fy7bEBy0Y7H7SbZJiVS88tPwS7Kbao_Kd0MN90UIXHRCfXvwoIMdrTlmlFAwA4WwDIFgasKXp589ATDtu9Kkkb1YI8fsYkeq-pe9jEz2DcNNGPUFRt1UbejIbGQM8KGZLB4bnbKMxipTx8STFABHsl3OQN4qOm2bbcu0WIE3OCzbRvTmK6KkOs9OY7nJzzJzzdt_UBQBXubeHHepNPhEy0OZ9S4UDuDcEo7Ow1dfi652iU2LRuDByAXYgaUHgmxYOp9uwBg4nPSRp2eJbiZ-qxE6baQXgU59GE7_a4QmTfphZID3FEoTSSzjzn0GaBTGtSIUbTv7DWtrt3E-h3hNF33PlrVqUEIf9CiOrTez1Rl3mjUfIzQLuv-2h3Lk88eejPH8cE_NiGq3gMxvuDxiwhJY2RRtT-IVIIGH9_j9RuwdF8DCcJNdzfX99BYJakE5lM9eUMgr3hMQsWyTp4-NbqsRk4=w338-h191-no" 
+            alt="D2 Leader" 
+            className="person"
+          />
+          <p><strong>David Dillard</strong></p>
+        </div>
+        <div className="col-md-6">
+          <img 
+            src="https://lh3.googleusercontent.com/wppCp-asU_xQAJLqCXlion-UQR013315JN2eB-darwk0mJj5r7Yi-BTHydzU6TYKIAbAqzU20B6rDm6W8lzsVwvgf6hhvCUO2BTksI1Hi3-cTDceTpZlojyFaIOKjRw1zYhcdSyA9YiddOF3HC-dTaPrCluRh8V8fyUpMeOovGZtgawXERzeu-WNL_7dU-uj0_Jo3vLYffRwGMX3ai7moTpbx4OKZZPleSBuAUrJHKUXB34DZFaD8mBS7LDaHptQg01lT2rt0JT5PMVg2pTLd5rpUcA6QcIwrl28dMRI9jyVYG32D7Za0CglOsBXH_hi6vU7C552LLIM22U_FnbkZGG1cVSjdSIlzUZ6TrcFa4o9-Vrf_okTPGGj0MGCsef_69ZDzEjnPgcdM1wgBsYWPlbEdwmORpUvzF7gqt8T-b1eXWEo2SbymsHMOiqcJYEcqNv9CHWoP152rvGPAhn_mcoTH7hwA_Ke-RB_x99sAyiWCLQUsJQTN4g4D4CKm0lu8doI37FYin0_UqTEjf5o8BsxMbTnX4HuCb-CpyVFrY14o0ABV1H4wXhGukBJhs4eoZTntLViTAF1EtSl9-buwVUNAdUE7ALy9djYMLk=w338-h191-no" 
+            alt="D2 Leader" 
+            className="person"
+          />
+          <p><strong>Doug Bissell</strong></p>
+        </div>
+        <div className="col-md-6">
+          <img 
+            src="https://lh3.googleusercontent.com/hlNpQqMgMbb8hFoOb2fePRxuonVKFXp0ry7n5nhlV2Z0B7L0IPhLco7utSSK_qyprkjrybUHLU2wqYEsW9i1Ydu9xMljnfNhS7ERsAgh-12KTIuexnqaBjZqj4aRYgg2AU5bmpkLppKaULmZHvuiYWDM_AF78FcIaZk5CzIyx0RYRJ6QR7QAMMWo0KJ2V3IRQdl8XCcNeQlhxet4du51zbb5OLAIgZ4NXPvbLtQGETSvqIPjKalg0UzuHIGzO3wKQSZJx5CS9btDs0EqKH2JolBRUGJcsYTo2Pn8m6vGEUgzBh0HHtvxdj_z1mTvph3jnUqkLklKQ9hZg61ZrB4-gzbeA0FMziOzXbrTUpN0CLRenccgaxc2zthrTbgMxkIUvlomvz1tc9RjF7-GyPX7lzN3fxMgF2ZCBM-ypaQbzRFlvHMJmCz7jO6TvRKGMvfAxbVe2jysN8L8cY2iv1kX5UYkKsYRFbWUPfF8712Me72h9UQVJk97LLQaS8heT3axAkTlVMgvgcCbt_gMy4UwztB-kpy60peMi3BPPvw5mLGFiarj3GZsLiDIkjiM4r-vCMN2Z0JY39LTzvo0xq3exBqTG-wwshE0sDe1jyo=w338-h191-no" 
+            alt="D2 Leader" 
+            className="person"
+          />
+          <p><strong>Grant Warner</strong></p>
+        </div>
+        <div className="col-md-6">
+          <img 
+            src="https://lh3.googleusercontent.com/5pE4oMoOfZN88phzoW2pw9uMnnQVpixPp9td2qrkbuKovey15whnfc8bivw7U-FvgvR1BwkVc1lPGq6POEAw5Lda6OfTILzycDfg9-AMhx2gKuKhQni10Mco6t_oG-zZklgoGIG8S39_khUutLh90vy0ZecpfqjuO4RYYsLSVjXhbWQhu8ekFz_8ARzQZiMyFTGHU8GxGKFWKxRj9wIclhWs_ZF4zFjQ4el7j2VEL8wdhstfM28l9aKhr543fhswZIz2y_SxFY-NIQmU7VA6b1Gx_NkoWPz6sVhUHtNv3h8s5NrubGZQ9WvORVDBAnQ74PTkuzBUuzresMBnCymIDTeuJWysbatPAWnWMlMOIPWcv22CruXz3swKmF6PoEfT9iCzPUfxp4Rt8cYt5blJH3l6LdTvIfbNHfAfwxQIIJghxouhUNRL0JsG7A3lASJm5CrJy0MrJXkLJsmQwWeAEpM9jUO9ySaHnNLrmu3FvqYO5-1PZGU9TAD1AzpLIHEiTytPXEy3Jf-3iAoyJ_-uwiAz2i8x2Ux8ZX-maq-pHv6W-gcFmcI_iJJl7UzVS58pkBD6AakSr_Y3uF22j2wuskfd0pELHR5LAbYx5wI=w338-h191-no" 
+            alt="D2 Leader" 
+            className="person"
+          />
+          <p><strong>Josh Williams</strong></p>
+        </div>
+        <div className="col-md-6">
+          <img 
+            src="https://lh3.googleusercontent.com/uVSzCr12_Z8G0MZt_TVPXeLe93t9yclqdWiUsoit7O6drYOUCrGlngl9aZxYq1JvZO-ieH4VSvCNJu174Yqy2ElN9an99OarNjLA2-lt40vkQ48mGTyC-ILVrMyIFOciZQjNAsYo9cK17hmaXyCjk4YnSWwk1SqP8PNs_V4diRtp52kti1JwuK0wawz-39gzRptG9iLU7Chg6J-g1O6yxR9rHOnwKEJhE0qn6Yfxi4oJHMpXolDO9fAFv_HfF_HHb6-AoScChJiHNe55SmKJ2pOBenGY0lBZI5dOtwAAAMpTLO9VXkYrekpfepL_izMFDY7A0-m_0slICiHyoyV1jexhcPS9SiEGZPNWyN6_pqafPqjP57e-D1G4XoJDoCRjBHvFEsWCNO6DoiUYe6WsUEyjUSrgcxe3CjHw1H-W57k59lvfEq92b1-ZFwk-ZKwCZGRifBJTO86JGys0fRv52oiwmA1DNLaTb2CFG9lAef8tBD1bUhsfUIv0We27Yr3-au-EpsZGj3Dn1Vs95tzF7myv606e9Yz_oeAo1zJedm4Qve3LLNBXMZSPzOw__dnocfI3rIm4oI8fKkHU-7YMB3BYADkAMFQkbqYfTCQ=w338-h191-no" 
+            alt="D2 Leader" 
+            className="person"
+          />
+          <p><strong>Keith Wilson</strong></p>
+        </div>
+        <div className="col-md-6">
+          <img 
+            src="https://lh3.googleusercontent.com/FZ9fqto3JCPtJJxScMg4OUSjsFNLZ66-QNvL-4OVsTJKWThnqSth1vHl7VsPnDe8A26GDt1CkKdMtUjiknzn02-P7hk6NAGU0YaHbuVfbU5ZmpjMJRqaBoY7X-CDFytVii6dl3VTt7xnZ9X6wG3u7ftEoUdCp3sRA7f_fCnAc9scQV7jKgVnBAO0oaP3Uy_ppc_sriCM6GUqvJN-I7PIZZM7Ji-k2-dwV9w9ySTIhFzau7P4qrNMkvmdDZrpMpjbjuHqjh4G2vW0eUX8YCuLhrP_j3mc2qj1ov7YhW-clAyjlFdDaN5gruxIoUOQVUEotsgNF8FzaApBzYD3gI1IuHvHig0dEZ5yXceEwStR6Ex2SUjSZKi6Y0O03MOW43uYWWYa1_L5NXg04V9DZ0Gn8YOGNzn5_7jKtCVgeNKnbJUi8IWozCUAjofGBNruOHcA9lL0YYMUt-M74hTMAOtzFa7YI-1YbXFCwOsRmgaBDQU-Ezm7Pf6IqUq2dF_L4eeu8bGO1DzTuJRqx9V_h-l3mCWHnniXWmC9iz4irUslzeKg0TOBldmKUoU_4gAWQGOUWSv32Lx2d9D7idl1y5hC3dfmlEJeKEWqSg0C4Co=w338-h191-no" 
+            alt="D2 Leader" 
+            className="person"
+          />
+          <p><strong>Siobhan Farvardin Winfrey</strong></p>
+        </div>
+        <div className="col-md-6">
+          <img 
+            src="https://lh3.googleusercontent.com/j-1E7-RDhDwfbHoPNrVvelUXquTQ2nLZw8b79xq2VDBKV-EOS9oY1H1v36hqN22bYHeTrf_13afZ5JvnfWSR9xKlVoKzr4zsUFBQJ_FdQJbAMkLMsufPsXnxgDwG1r73lOKNG3RPjR1sf6-MbFbgvw0WtfKD3UZumOyK-W3yC9ON2T2Dk91sTDWTaaCAet2YywwazWRGX3vlhTOMXH1nXNFc6IfBlTO5rJCq92YLrjNrmTjiwnIxboRQC7ecFFirv2uMgggMaIiBs4lilNvBhgZIQVq9m2xLNDay0p6c9yLKSiJ3AzgtxKHcjVguSmaLH4bz3lqWOG2Y24ciCQxHIUAUkKNN4G7BimbRgUPZRnFYl-yytm_T6azSuwNdj9qULdZ1_61x_-CsLX_ritkmd0InwlhRbK5npAkqpmla2LlJW36fsX1iu3puAcgnEX7DEyzRyZW7l6-irKBcxWqtl9Ai2J6f-VjAHJUd7V0AFB5_icbM39vVoHef-zyo5xFwf65le7e3cmJexrtOGnDxvf5JcCXtPI5gQkyDYF7xcztlWiaO3gB6Int4n-j1urr6nWvcshTH8VxopUnbckpSKxI7-1Vo5qYleFX5ZS4=w338-h191-no" 
+            alt="D2 Leader" 
+            className="person"
+          />
+          <p><strong>Gerry Hicks</strong></p>
+        </div>
+        <div className="col-md-6">
+          <img 
+            src="https://lh3.googleusercontent.com/l2A7sTogjLMKjcFztvy3AvV0-emmswKzIF9WqJC00cluorCut7U8P42VYMVTTl2QMANhRag6f_YC0I6A_SiCzVAhzJlYB9k1ymYmwUNZdROCKtfIv5I0y8SMHGKubLPVp8azHRrjkAmDwjS28FAKc4fb03vCAVQv5AaWQ_pt1ox4A0TBdW2tK9GZbFlUEqkwBE4cmrzcXI5yCSbt8J4h7eaObGPE_SjgM0HKxfETBIyWtc0EJHG9Gt8kiCNRfPurmQ08R9Z_DdTiETxEtjq4iuHyW9au4rizodGgbiC22GwRICtWLzCDm6Xzc-ibkwtWNKfAnkp2ttAuRl8hd0fO8Y4wrelrtRIl1IwmhDGo0eymPKGEEkbDkedqjOw52adGF9eSvVpLQPhMb7EWVy_XX_Xb3aPDvUuyJ-4ZdWAEC4jpnZw27TDm8EDXsfZmirFaVr6QIYql92mU2Kc2hRymHyQ8b9TWVX8RgN7dLGZLukIXr1QYcHqhdmKlNOmwpzCWUugnOXGOlMMjrms377w46S9WkqO5505ILEfvi9y62rf2CKDyEcP4bd4g22D_zKjoBOYYbhJJYJ3XefyO3xskuHm6xmwIDBuDz5i1Nfo=w338-h191-no" 
+            alt="D2 Leader" 
+            className="person"
+          />
+          <p><strong>James Hamilton</strong></p>
+        </div>
       </div>
     );
   }
