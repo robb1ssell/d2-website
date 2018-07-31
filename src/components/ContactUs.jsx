0f34645e96106367cea650d0e2a932b3c5755c57
Rob Bissell <roblincolnbissell@gmail.com>
2018-07-30 22:20:06 -0500

    beginning contact us page done

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -3,8 +3,48 @@ import React, { Component } from 'react';
 class ContactUs extends Component {
   render() {
     return (
-      <div>
-        <h1>contact us page</h1>
+      <div className='col-md-6'>
+        <h1>Contact Us</h1>
+        <img 
+          src="https://lh3.googleusercontent.com/D2l_v_mXeW0SWqI02EutTkG9zQhjjkn3Wa1aoINbmulxEJTLCe_cUcxKZC-NJuflQzxzMQA7sQgpmqrxesfoTf_Pog61ue3wPiU12QioDRs0FgXdg3yEQhWMjXiXSJCoG1ysbnPlD9FAttL3yBB-_7hbYDKH9Ok4Ul8ojVcFhRz4rFyNsCk2-OngPWVo-eVVyw597e8nl-ftoP2QN3WCG3FTYMPVMb_5IaLbKKU1BRA_DX6kV6zrYfqUhSfo2RTktsMHoLPVcYOSe2RFdyq0gqOE7mDMAZX5opEF5tu-VJz9qZlrVKqFeZp-ICqB3gXiPswRr42OQjRvgOvoxKHKZ9Ga28ry3t35EA5zF5KCiNOA6GbAlQG3Biy15s-E4mOL8DaEiDM2prOEq9lDptqWX_pH-bSAOZXB46gW-H9aTWRFwbGoZne6NVah5QpfUqQsgEUEfvLxNjP-20ZGPazOBzEEvLEPQE6LqLFg8Em-gr9yqFX-BqRTTdCCAx09UqzZUe_RDuPT5LWz6N3sLzgWafvhA0ElMitCLvdk_U9Eho_uKwW7lu21aD57lfmobrG7gDWIzHZwyKy6iG8hgOsOTsdu0ab124H9aGwdUSg=w500-h333-no" 
+          alt="D2 Group Photo"
+          className='fit-image'
+        />
+
+        <form action="mailto:robertb.webdev@gmail.com" method='post'>
+          <input 
+            type="text" 
+            name="name" 
+            id="name"
+            maxLength='50'
+            placeholder='Name'
+          />
+          <input 
+            type="text" 
+            name="email" 
+            id="email"
+            maxLength='50'
+            placeholder='E-Mail Address'
+          />
+          <input 
+            type="text" 
+            name="phone" 
+            id="phone"
+            maxLength='12'
+            placeholder='Phone Number'
+          />
+          <textarea 
+            name="message" 
+            id="message" 
+            cols="30" 
+            rows="10"
+            placeholder='Message'
+          >
+          </textarea>
+          <center>
+            <input type="submit" value='Submit'/>
+          </center>
+        </form>
       </div>
     );
   }
